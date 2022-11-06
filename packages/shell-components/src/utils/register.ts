363c5a4aed0a0d71df275f2bde3f90eac8e52e3b
Gj <gongjie0422@163.com>
2022-11-06 20:03:55 +0800

    fix: components

--- packages/shell-components/src/utils/register.ts
+++ packages/shell-components/src/utils/register.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
-import type { Component } from 'vue'
+import type { App, Component } from 'vue'
 import { ShellComponentContainer } from "@takeover/shared-utils";
 
+let swapApp: App | null = null
+
 export function registerEl(target: Component) {
-    createApp(target).mount(`#${ShellComponentContainer}`)
+    if (swapApp) swapApp.unmount()
+    swapApp = createApp(target)
+    swapApp.mount(`#${ShellComponentContainer}`)
 }
